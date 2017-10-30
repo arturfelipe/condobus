caa480919489296e2d537fc5e70c8647d97d2d39
Artur Sousa <arturfelipe.sousa@gmail.com>
2017-10-30 01:05:41 -0200

    Create a bus model

--- transport/models.py
+++ transport/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Bus(models.Model):
+    name = models.CharField(_('Nome'), max_length=200)
+
+    class Meta:
+        verbose_name = _('Ônibus')
+
+    def __str__(self):
+        return self.name
