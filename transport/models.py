5259912e7aa274aee57c2b02e457ca56b236e203
Artur Sousa <arturfelipe.sousa@gmail.com>
2017-10-31 23:53:32 -0200

    Add an organization relation to the bus model

--- transport/models.py
+++ transport/models.py
@@ -4,6 +4,12 @@ from django.utils.translation import ugettext_lazy as _
 
 class Bus(models.Model):
     name = models.CharField(_('Name'), max_length=200)
+    organization = models.ForeignKey(
+        'org.Organization',
+        on_delete=models.CASCADE,
+        related_name='buses',
+        verbose_name=_('Organization')
+    )
 
     class Meta:
         verbose_name = _('Bus')
