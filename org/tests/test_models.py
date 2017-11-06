ab4c2e165f7dabb3304d6f650c9aaa43490b4140
Artur Sousa <arturfelipe.sousa@gmail.com>
2017-11-06 00:56:05 -0200

    Add some organization tests

---
+++ org/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from ..models import Organization
+
+
+class OrganizationModelTest(TestCase):
+
+    def setUp(self):
+        self.org = Organization.objects.create(
+            name='Some Org',
+            logo='/media/logos/some-org-logo.jpg',
+            description='We are a familiar condominium',
+            rules='Please check our conduct code page at https://some-url.foo'
+        )
+        self.org.save()
+
+    def test_str(self):
+        self.assertEqual('Some Org', str(self.org))
+
+    def test_can_create(self):
+        self.assertTrue(Organization.objects.exists())
