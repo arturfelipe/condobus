5c7a850d83b1740ea34a74797b7b50c111f7bb25
Artur Sousa <arturfelipe.sousa@gmail.com>
2017-11-05 23:12:34 -0200

    Create a Route model

--- transport/models.py
+++ transport/models.py
@@ -18,3 +18,20 @@ class Bus(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Route(models.Model):
+    name = models.CharField(_('Name'), max_length=200)
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name='routes',
+        verbose_name=_('Organization')
+    )
+
+    class Meta:
+        verbose_name = _('Route')
+        verbose_name_plural = _('Routes')
+
+    def __str__(self):
+        return self.name
