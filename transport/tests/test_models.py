ce2855d82331fc7bb1ffdb07761d6ad235a1c6c9
Artur Sousa <arturfelipe.sousa@gmail.com>
2017-11-07 23:54:25 -0200

    Add some Route model tests

--- transport/tests/test_models.py
+++ transport/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from org.models import Organization
-from ..models import Bus
+from ..models import Bus, Route
 
 
 class BusModelTest(TestCase):
@@ -23,3 +23,24 @@ class BusModelTest(TestCase):
 
     def test_can_create(self):
         self.assertTrue(Bus.objects.exists())
+
+
+class RouteModelTest(TestCase):
+
+    def setUp(self):
+        self.org = Organization.objects.create(
+            name='Some Org',
+            logo='/media/logos/some-org-logo.jpg',
+            description='We are a familiar condominium',
+            rules='Please check our conduct code page at https://some-url.foo'
+        )
+        self.route = Route.objects.create(
+            name='Route 1',
+            organization=self.org
+        )
+
+    def test_str(self):
+        self.assertEqual('Route 1', str(self.route))
+
+    def test_can_create(self):
+        self.assertTrue(Route.objects.exists())
