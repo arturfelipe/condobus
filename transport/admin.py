59f81c78e5d36614e0f187e4e95cc3924a0b07e1
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-01-02 14:12:54 -0200

    Add a StopPoint model

--- transport/admin.py
+++ transport/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
-from transport.models import Bus, Route
+from django.contrib.gis import admin
+from transport.models import Bus, Route, StopPoint
 
 admin.site.register(Bus)
 admin.site.register(Route)
+admin.site.register(StopPoint, admin.OSMGeoAdmin)
--- transport/models.py
+++ transport/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
 from django.utils.translation import ugettext_lazy as _
 from org.models import Organization
 
@@ -15,6 +16,19 @@ class Bus(models.Model):
     class Meta:
         verbose_name = _('Bus')
         verbose_name_plural = _('Buses')
+
+    def __str__(self):
+        return self.name
+
+
+class StopPoint(models.Model):
+    name = models.CharField(_('Name'), max_length=200)
+    location = models.PointField(_('Location'), default=Point(-22.99977, -43.36063))
+    obs = models.TextField(_('Observation'), null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('StopPoint')
+        verbose_name_plural = _('StopPoints')
 
     def __str__(self):
         return self.name
